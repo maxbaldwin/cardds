eca0e85597f29e6a9b8d233dd464f6e73147bbee
Max Baldwin <maxrbaldwin@gmail.com>
2016-03-05 13:32:45 -0600

    added the text stuff

--- cards.js
+++ cards.js
@@ -2,6 +2,11 @@
 var create = document.getElementById("create");
 var del = document.getElementById("delete");
 var input = document.getElementById("input");
-var container = document.getElementById("container");
+var container = document.getElementById("container"); //output
 var card = document.getElementsByClassName("card"); //returns an arry
-console.log(card); //should be empty at first
+// console.log(card); //should be empty at first when you console log it
+
+create.addEventListener("click", createCard);
+  function createCard() {
+    var output = '<div class="card"><section><p class="text">' + input.value + '</p></section><input type="button" class="delete" value="Delete"></div>';container.innerHTML += output
+}
