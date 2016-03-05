ff2d2c009f7701618fe7b286608e9a59eafc4514
Max Baldwin <maxrbaldwin@gmail.com>
2016-03-05 12:41:54 -0600

    adding variables

--- cards.js
+++ cards.js
@@ -1,1 +1,7 @@
-change
+// creating list of variables that will be referenced 
+var create = document.getElementById("create");
+var del = document.getElementById("delete");
+var input = document.getElementById("input");
+var container = document.getElementById("container");
+var card = document.getElementsByClassName("card"); //returns an arry
+console.log(card); //should be empty at first
